76f1bdea011aae4543488fa09301d9858ec91a6a
jijiseong <jiseong5036@gmail.com>
2025-05-23 09:22:18 +0900

    chore: version update test

--- app.config.js
+++ app.config.js
@@ -1,40 +1,40 @@
 const getPackageName = () => {
   switch (process.env.APP_ENV) {
-    case 'development':
-      return 'com.spoclipai.gymkioskapp.dev';
-    case 'preview':
-      return 'com.spoclipai.gymkioskapp.preview';
-    case 'production':
+    case "development":
+      return "com.spoclipai.gymkioskapp.dev";
+    case "preview":
+      return "com.spoclipai.gymkioskapp.preview";
+    case "production":
     default:
-      return 'com.spoclipai.gymkioskapp';
+      return "com.spoclipai.gymkioskapp";
   }
 };
 
 const getAppName = () => {
   switch (process.env.APP_ENV) {
-    case 'development':
-      return '스포클립 체육관용(dev)';
-    case 'preview':
-      return '스포클립 체육관용(preview)';
-    case 'production':
+    case "development":
+      return "스포클립 체육관용(dev)";
+    case "preview":
+      return "스포클립 체육관용(preview)";
+    case "production":
     default:
-      return '스포클립 체육관용';
+      return "스포클립 체육관용";
   }
 };
 
 module.exports = ({ config }) => ({
   ...config,
   name: getAppName(),
-  slug: 'gym-kiosk-app',
-  version: '1.0.1',
-  orientation: 'landscape',
-  icon: './assets/icon.png',
-  userInterfaceStyle: 'light',
+  slug: "gym-kiosk-app",
+  version: "1.0.2",
+  orientation: "landscape",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
   newArchEnabled: true,
   splash: {
-    image: './assets/splash-icon.png',
-    resizeMode: 'contain',
-    backgroundColor: '#ffffff',
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
   },
   ios: {
     ...config.ios,
@@ -45,27 +45,27 @@ module.exports = ({ config }) => ({
     package: getPackageName(),
   },
   web: {
-    favicon: './assets/favicon.png',
+    favicon: "./assets/favicon.png",
   },
   plugins: [
     [
-      'expo-screen-orientation',
+      "expo-screen-orientation",
       {
-        initialOrientation: 'LANDSCAPE',
+        initialOrientation: "LANDSCAPE",
       },
     ],
-    'expo-asset',
+    "expo-asset",
   ],
   extra: {
     eas: {
-      projectId: '0b6fecd2-acdd-4bee-843d-46da1f310d49',
+      projectId: "0b6fecd2-acdd-4bee-843d-46da1f310d49",
     },
   },
-  owner: 'spoclip-ai',
+  owner: "spoclip-ai",
   updates: {
-    url: 'https://u.expo.dev/0b6fecd2-acdd-4bee-843d-46da1f310d49',
+    url: "https://u.expo.dev/0b6fecd2-acdd-4bee-843d-46da1f310d49",
   },
   runtimeVersion: {
-    policy: 'appVersion',
+    policy: "appVersion",
   },
 });
